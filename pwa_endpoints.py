c65bdec7702012bc2947ddb1e3df55a2325ed784
AGQuant <arpitgoel075@gmail.com>
2026-06-28 16:42:31 +0530

    cc#109: rename Screener -> V12 in canonical desktop nav label

--- pwa_endpoints.py
+++ pwa_endpoints.py
@@ -232,7 +232,7 @@ PWA_JS = """
       ['/check', '\\u2713', 'Check'],
       ['/news', '\\ud83d\\udcf0', 'Intel'],
       ['/scanners', '\\u229e', 'Scanners'],
-      ['/screener', '\\u229f', 'Screener'],
+      ['/screener', '\\u229f', 'V12'],
       ['/cio', '\\u2299', 'Max'],
       ['/fpc', '\\u25e7', 'FPC'],
       ['/quant-basket', '\\u25eb', 'QB'],
